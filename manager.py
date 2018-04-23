1cc259789d16c555d442ecdbc11fcdc0bcf51f95
urielmalave <urielmalave@upr.edu>
2018-04-23 08:38:58 -0400

    Last commits

--- manager.py
+++ manager.py
@@ -11,31 +11,31 @@ reader = PyPDF2.PdfFileReader(pdf)
 
 images = encoder.encode(reader.getPage(0))
 
-# print(len(images))
-#
-# for image in images:
-#     print(image)
-#     print()
+print(len(images))
+
+for image in images:
+    print(image)
+    print()
 
 # print("******NEXT PAGE******")
 
 
 output = PyPDF2.PdfFileWriter()
 
-for index in len(reader.pages):
-    page = encoder.merge(page, images)
-    output.addPage(page)
+# for index in len(reader.pages):
+#     page = encoder.merge(page, images)
+#     output.addPage(page)
 
 outputStream = open("optimized.pdf", "wb")
 output.write(outputStream)
 
 # images2 = encoder.encode(page)
 #
-# # print(len(images2))
-# #
-# # for image in images2:
-# #     print(image)
-# #     print()
+# print(len(images2))
+#
+# for image in images2:
+#     print(image)
+#     print()
 # output.addPage(page)
 
 # print(len(xos))
@@ -44,15 +44,13 @@ output.write(outputStream)
 #     print(xo)
 #     print()
 
-# for img in images:
-#     img_extractor.extractImages(img)
+for img in images:
+    img_extractor.extractImages(img)
 
 
-# output = PyPDF2.PdfFileWriter()
-# # output.addPage(reader.getPage(0))
+output = PyPDF2.PdfFileWriter()
+output.addPage(reader.getPage(0))
 
 # for page in reader.pages:
 #     encoder.merge(page, images)
 #     output.addPage(page)
-
-
